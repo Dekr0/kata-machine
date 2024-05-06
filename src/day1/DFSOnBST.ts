792d8dfcd952794ddc2a38e0329b05f1b0a6e928
Dekr0 <dekr0.dk@protonmail.com>
2024-05-06 05:24:23 -0600

    remove day1

--- src/day1/DFSOnBST.ts
+++
@@ -1,6 +0,0 @@
-export default function dfs(head: BinaryNode<number>, needle: number): boolean {
-    if (head.left === null && head.right === null) return head.value === needle;
-
-    return (head.left !== null && dfs(head.left, needle)) || 
-            (head.right !== null && dfs(head.right, needle));
-}
